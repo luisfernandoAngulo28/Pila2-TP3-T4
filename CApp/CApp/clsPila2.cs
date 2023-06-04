bccb8ef078c5dc1f31e418244a1f82b9804e6151
Fernando Angulo <57889477+luisfernandoAngulo28@users.noreply.github.com>
2023-06-03 23:50:18 -0400

    Grupo SI
    Grupo SI

---
+++ CApp/CApp/clsPila2.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CApp
+{
+    public class clsPila2
+    { //Atributos
+        public const int MAX = 100;
+        private int[] Pila = new int[MAX];
+        public int top1;
+        public int top2;
+        public int cant;// cantidad de elementos en el arreglo
+        //Constructor
+
+        public clsPila2()
+        {
+            top1 = -1;
+            top2 = MAX;
+            cant = 0;
+            for (int i = 0; i < MAX; i++)
+            {
+                Pila[i] = 0;
+            }
+        }
+
+        public clsPila2(clsPila2 p)
+        {
+            top1 = p.top1;
+            top2 = p.top2;
+            cant = p.cant;
+            for (int i = 0; i < MAX; i++)
+            {
+                Pila[i] = p.Pila[i];
+            }
+        }
+        //Llena(S) -->  Boolean        
+        // Retorna TRUE si S esta llena de lo contrario FALSE
+        public bool Llena(int S)
+        {
+            bool b = false;
+            if (S == 1)
+            {
+                if (top1 + 1 == MAX / 2)
+                {
+                    b = true;
+                }
+            }
+            else
+            {
+                if (top2 - 1 == MAX / 2)
+                {
+                    b = true;
+                }
+            }
+            return b;
+        }
+
+        //Vacia(S) --> Boolean       
+        // Retorna TRUE si S esta vacía de lo contrario FALSE
+        public bool Vacia(int S)
+        {
+            bool b = false;
+            if (S == 1)//pila1
+            {
+                if (top1 == -1)
+                {
+                    b = true;
+                }
+            }
+            else
+            {//pila 2
+                if (top2 == MAX)
+                {
+                    b = true;
+                }
+            }
+            return b;
+        }
+        //Add(S,x) -->S 
+        // Adiciona el elemento x la pila,
+        public void Add(int S, int x)
+        {
+            if (S == 1)//pila1
+            {
+                if (!Llena(S))
+                {
+                    top1++;
+                    Pila[top1] = x;
+                    cant++;
+                }
+            }
+            else
+            {
+                if (!Llena(S))
+                {
+                    top2--;
+                    Pila[top2] = x;
+                    cant++;
+                }
+            }
+        }
+
+        // Delete(S) -->S 
+        // Elimina el elemento tope de la pila
+
+        public void Delete(int S)
+        {
+            if (S == 1)
+            {
+                if (!Vacia(S))
+                {
+                    top1--;
+                }
+            }
+            else
+            {
+                if (!Vacia(S))
+                {
+                    top2++;
+                }
+            }
+        }
+
+        //Mostrar
+
+        public String Mostrar()
+        {
+            String S = "P[";
+            for (int i = 0; i <= top1; i++)
+            {
+                S = S + Pila[i] + "|";
+            }
+            for (int i = top2; i < MAX; i++)
+            {
+                S = S + Pila[i] + "|";
+            }
+            S = S.Substring(0, S.Length - 1);//quita la ultimo palito
+            return S + "]";
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ WApp/WApp/FormularioPila2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CApp;
+
+
+namespace WApp
+{
+    public partial class FormularioPila2 : Form
+    {
+        public FormularioPila2()
+        {
+            InitializeComponent();
+        }
+        //instanciamos la clase
+        clsPila2 p = new clsPila2();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int s = 1;
+            int x = int.Parse(textBox1.Text);
+            p.Add(s, x);
+            textBox3.Text = p.Mostrar();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int s = 1;
+            p.Delete(s);
+            if (p.Vacia(s)==true)
+            {
+                MessageBox.Show("Pila S1 Vacia");
+            }
+            else
+            {
+                textBox3.Text = p.Mostrar();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int s = 2;
+            int x = int.Parse(textBox2.Text);
+            p.Add(s, x);
+            textBox3.Text = p.Mostrar();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int s = 2;
+            p.Delete(s);
+            if (p.Vacia(s) == true)
+            {
+                MessageBox.Show("Pila S1 Vacia");
+            }
+            else
+            {
+                textBox3.Text = p.Mostrar();
+            }
+        }
+    }
+}
